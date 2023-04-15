235c2eb21be273cca44ddb34105bf7f557f1f41b
Bruno <brunoalves.ds95@gmail.com>
2023-04-15 01:59:54 -0300

    Add styles to the pages

--- src/app/services/personService/person.service.ts
+++ src/app/services/personService/person.service.ts
@@ -2,16 +2,24 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Person } from '../../models/Person';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PersonService {
-  private baseUrl = 'https://crudcrud.com';
+  private baseUrl = 'https://crudcrud.com/api/b5132150d5b6415f91ece649b7140e4d';
 
   constructor(private http: HttpClient) { }
 
-  getPeople(id: any): Observable<Person[]> {
+  getPeopleList(): Observable<Person[]> {
+    return this.http.get<Person[]>(`${this.baseUrl}/people`).pipe(
+      map((people: Person[]) => people)
+    );
+  }
+
+
+  getPerson(id: any): Observable<Person[]> {
     const url = `${this.baseUrl}/people`;
     const params = new HttpParams().set('id', id.toString());
     return this.http.get<Person[]>(url, { params });
@@ -22,10 +30,10 @@ export class PersonService {
   }
 
   updatePerson(person: Person): Observable<Person> {
-    return this.http.put<Person>(`${this.baseUrl}/people/${person.id}`, person);
+    return this.http.put<Person>(`${this.baseUrl}/people/${person._id}`, person);
   }
 
-  deletePerson(id: number): Observable<void> {
+  deletePerson(id: string): Observable<void> {
     return this.http.delete<void>(`${this.baseUrl}/people/${id}`);
   }
 }
