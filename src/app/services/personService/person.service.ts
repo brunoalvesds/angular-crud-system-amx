feec3e9abca4e96b0c02b4392d9d0e02ba025617
Bruno <brunoalves.ds95@gmail.com>
2023-04-14 23:26:29 -0300

    Create and update person, services base

--- src/app/services/personService/person.service.ts
+++ src/app/services/personService/person.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Person } from '../../models/Person';
 
@@ -11,8 +11,10 @@ export class PersonService {
 
   constructor(private http: HttpClient) { }
 
-  getPeople(): Observable<Person[]> {
-    return this.http.get<Person[]>(`${this.baseUrl}/people`);
+  getPeople(id: any): Observable<Person[]> {
+    const url = `${this.baseUrl}/people`;
+    const params = new HttpParams().set('id', id.toString());
+    return this.http.get<Person[]>(url, { params });
   }
 
   createPerson(person: Person): Observable<Person> {
